738791f976cbd0dbbd06161d4bc5c3b7ea9f1f95
mohammad <khezri@outlook.com>
2018-10-02 21:04:47 -0500

    json

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,6 @@
-import {charactersBuilder} from './components/characters.js'
-
+import {getCharacters} from './data/charactersData.js';
 const initializeApp = () => {
-  charactersBuilder()
+  getCharacters();
 };
 
 initializeApp();
